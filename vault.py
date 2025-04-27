90bdc1100cab5c61eb221639029a0d716102eee6
Brandon Saldanha <bbbsaldanha@gmail.com>
2025-04-27 13:58:53 -0500

    Automatically generate the key

--- vault.py
+++ vault.py
@@ -1,11 +1,26 @@
+import os
 import json
 from cryptography.fernet import Fernet
 from entry import Entry
 
 class Vault:
-    def __init__(self, owner: str):
+    def __init__(self, owner: str, key_file="encruption.key"):
         self.owner = owner
         self.entries = []
+        self.key_file = key_file
+
+        #load the encryption key from file or generate a new one if it doesn't exist
+        self.encryption_key = self.load_or_generate_key()
+
+    def load_or_generate_key(self):
+        if os.path.exists(self.key_file):
+            with open(self.key_file, 'rb') as key_file:
+                key = key_file.read()
+        else:
+            key = Fernet.generate_key()
+            with open(self.key_file, 'wb') as key_file:
+                key_file.write(key)
+        return key
 
     def add_entry(self, site: str, username: str, password: str):
         entry = Entry(site, username, password)
@@ -21,3 +36,12 @@ class Vault:
         encrypted = fernet.encrypt(data)
         with open(filepath, 'wb') as f:
             f.write(encrypted)
+    
+    def load(self, filepath: str):
+        """Load the vault from a file, decrypting the data with the stored encryption key."""
+        fernet = Fernet(self.encryption_key)
+        with open(filepath, 'rb') as f:
+            encrypted = f.read()
+        decrypted_data = fernet.decrypt(encrypted)
+        entries_data = json.loads(decrypted_data.decode())
+        self.entries = [Entry(**entry) for entry in entries_data] # Creates a list of Entry objects from the loaded dictionary data, unpacking each dictionary into the Entry constructor
